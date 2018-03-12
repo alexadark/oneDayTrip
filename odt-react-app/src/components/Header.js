3c45c997f243c34907931f292f7704f7b303ea80
Alexa Spalato <alexaspalato@gmail.com>
2018-03-12 11:31:04 +0100

    header component

--- odt-react-app/src/components/Header.js
+++ odt-react-app/src/components/Header.js
@@ -2,11 +2,20 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <h1>One Day Trips</h1>
-        <NavLink className="uk-button uk-button-primary" to="/" activeClassName="is-active" exact={true}>Home </NavLink>
-        <NavLink to="/publish" activeClassName="is-active">Publish</NavLink>
-        <NavLink to="/sign-up" activeClassName="is-active">Sign Up</NavLink>
+    <header className="uk-padding-small">
+        <div className="uk-container uk-flex uk-flex-between">
+            <NavLink to="/"
+                     activeClassName="is-active"
+                     exact={true}><h2>One Day Trips</h2></NavLink>
+            <nav>
+                <NavLink className="uk-button uk-button-small uk-button-primary uk-margin-small-right" to="/publish"
+                         activeClassName="is-active">Publish</NavLink>
+                <NavLink className="uk-button uk-button-small uk-button-primary uk-margin-small-right" to="/sign-up"
+                         activeClassName="is-active">Sign Up</NavLink>
+            </nav>
+        </div>
+
+
     </header>
 );
 
